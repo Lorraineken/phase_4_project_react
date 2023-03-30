089e5eb77b712d7f9f5ef2206159812595b3b1a6
Alex Tanui <tanuialex417@gmail.com>
2023-03-30 12:49:03 +0300

    updated styling for nav

---
+++ src/componets/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import backgroundImage from '../images/backgroundnew.jpg';
+import './Home.css'
+const HomePage = () => {
+  return (
+    <div style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Palm<br/>Fitness<br/>Hub<span>.</span></h1>
+        <h2>no pain no gain</h2>
+        <button type="button" class="btn btn-light">Join us today</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
