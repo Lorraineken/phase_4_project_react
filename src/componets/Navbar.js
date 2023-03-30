5aca9bc341cd7082a38c0626b271084511d30424
Alex Tanui <tanuialex417@gmail.com>
2023-03-30 12:49:21 +0300

    redo home

---
+++ src/componets/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">About Us</Link>
+        </li>
+        <li>
+          <Link to="/program">Program</Link>
+        </li>
+        <li>
+          <Link to="/login">Log-in</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
