b2c7dbcbf21afcadc727f801c3c44ce4093f362f
Lorraineken <lorrainekupa@gmail.com>
2023-03-31 12:42:21 +0300

    Added signup component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 // import Navbar from './componets/Navbar';
 import Header from './components/Header'
 import Home from './components/Home';
+import Signup from './components/Signup';
+
+
+
 function App() {
   return (
     <div className="App">
