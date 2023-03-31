cb353b8025c86880e2629870059b4dd3ea7c4889
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-31 11:38:07 +0300

    updated components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import Navbar from './componets/Navbar';
-import Header from './componets/Header'
-import Home from './componets/Home';
+// import Navbar from './componets/Navbar';
+import Header from './components/Header'
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
