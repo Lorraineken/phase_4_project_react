89a5fdc90389fd843e7c193ca69b1fdb826f0f11
Alex Tanui <tanuialex417@gmail.com>
2023-04-02 22:51:59 +0300

    added color

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './UserNav.css';
 
 function UserNav() {
   return (
-    
-      <div className="navbar navbar-light bg-">
-
-        <Link className="navbar-brand" to="/">PalmGym</Link>
-        
-      
-              <Link className="nav-link" to="/workout">Workout</Link>
-            
-            
-              <Link className="nav-link" to="/dashboard">My Workout</Link>
-            
-         
-          
-              <Link className="nav-link" to="/logout">Logout</Link>
-           
-        
-      </div>
-    
+    <div className="navbar navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">PalmGym<span>.</span></Link>
+      <Link className="nav-link" to="/workout">Workout</Link>
+      <Link className="nav-link" to="/dashboard">My Workout</Link>
+      <Link id="logout-btn" className="btn btn-secondary" to="/logout">Logout</Link>
+    </div>
   );
 }
 
